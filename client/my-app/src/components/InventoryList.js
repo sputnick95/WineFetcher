c51e862190d955e12007874226814514d28b65ce
zoe petropoulou <zoepetropoulou@zoes-MacBook-Air-3.local>
2023-04-06 17:18:34 -0400

    added pagination

--- client/my-app/src/components/InventoryList.js
+++ client/my-app/src/components/InventoryList.js
@@ -4,30 +4,25 @@ import { Pagination } from 'react-bootstrap';
 //Child Components
 import InventoryItem from './InventoryItem'
 
-
 function InventoryList(inventory){
-
-    console.log(inventory.inventory.white_wines)
+    const [activePage, setActivePage] = useState(1);
     const ww = inventory.inventory.white_wines
+    const pageSize = 9;
+    let totalPages;
+    let productsOnPage;
+    let startIndex;
 
-    let wineries;
-        if (ww !== undefined) {
-            wineries = ww.map((item) => item.winery)
-            // console.log(wineries)
-        }
+    if (ww !== undefined) {
+        totalPages = Math.ceil(Object.keys(ww).length / pageSize);
+        startIndex = (activePage - 1) * pageSize;
+        productsOnPage = ww.slice(startIndex, startIndex + pageSize);
+    }
+    
+    const handlePageChange = (pageNumber) => {
+        setActivePage(pageNumber)
+    };
 
-    // const [activePage, setActivePage] = useState(1);
-    // const pageSize = 10;
-    // const totalPages = Math.ceil(inventory.length / pageSize);
-    // const startIndex = (activePage - 1) * pageSize;
-    // const productsOnPage = inventory.slice(startIndex, startIndex + pageSize);
-
-
-    // figure out how to get length of object
-    console.log(typeof wineries)
-        
-        
-
+    console.log(productsOnPage)
 
 
     return(
@@ -36,12 +31,18 @@ function InventoryList(inventory){
                 <h1>Inventory</h1>
             </div>
             <div className="cards">
-                {ww !== undefined ? ww.map((item) => <InventoryItem {...item} key={item.id}/> )  : null }
+                {productsOnPage !== undefined ? productsOnPage.map((item) => <InventoryItem {...item} key={item.id}/> )  : null }
             </div>
-
+            <Pagination className="justify-content-center" >
+                {Array.from({ length: totalPages}, (_, i) => (
+                    <Pagination.Item key={i} active={i + 1 === activePage} onClick={() => handlePageChange(i +1)}>
+                        {i + 1}
+                    </Pagination.Item>
+                ))}
+            </Pagination>
         </>
     )
 }
-
 export default InventoryList;
 
+ 
---
+++ client/my-app/src/components/Item_Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Item_Details(){
+    
+
+    
+    return(
+        <div>
+
+        </div>
+    )
+}
