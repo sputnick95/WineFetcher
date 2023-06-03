481779de1786852ff51a83e0ad5fa80414b13bd7
zoe petropoulou <zoepetropoulou@zoes-MacBook-Air-3.local>
2023-06-03 00:25:01 -0400

    chasing stars

--- client/my-app/src/components/ItemDetails.js
+++ client/my-app/src/components/ItemDetails.js
@@ -1,45 +1,49 @@
-import React, {useState, useMemo, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 
-const Star = ({ starId, rating, onMouseEnter, onMouseLeave, onClick }) => {
-    let styleClass = "star-rating-blank";
-    if (rating && rating >= starId) {
-      styleClass = "star-rating-filled";
-    }
-   
-    return (
-      <div
-        className="star"
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        onClick={onClick}
+const Star = ({ starId, rating }) => {
+  const styleClass =
+    rating && rating - starId > 1
+      ? "star-rating-filled"
+      : rating && rating - starId >= 0.5
+      ? "half-star-rating"
+      : "star-rating-blank";
+
+  return (
+    <div className="star">
+      <svg
+        height="55px"
+        width="53px"
+        className={styleClass}
+        viewBox="0 0 25 23"
+        data-rating="1"
       >
-        <svg
-          height="55px"
-          width="53px"
-          class={styleClass}
-          viewBox="0 0 25 23"
-          data-rating="1"
-        >
-          <polygon
-            stroke-width="0"
-            points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"
-          />
-        </svg>
-      </div>
-    );
-  };
+        <defs>
+          <clipPath id="halfStarClip">
+            <polygon points="0,0 9.7,0 10.5,23 0,23" />
+          </clipPath>
+        </defs>
+        <polygon
+          strokeWidth="0"
+          points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"
+          clipPath={styleClass === "half-star-rating" ? "url(#halfStarClip)" : undefined}
+        />
+      </svg>
+    </div>
+  );
+};
+
 
 
 function ItemDetails({selectedItem}){
+
+
     const [rating, setRating] = useState(0);
-    const [hoverRating, setHoverRating] = useState(0);
     const stars = [1, 2, 3, 4, 5];
+
 
     useEffect(() => {
         if (selectedItem) {
@@ -47,9 +51,7 @@ function ItemDetails({selectedItem}){
         }
       }, [selectedItem]);
 
-
-    console.log(selectedItem?.average_rating)
-    console.log(selectedItem)
+    
 
     return(
         <>
@@ -58,7 +60,7 @@ function ItemDetails({selectedItem}){
                 <div className='item-details-div'>
                     <Col>
                         <div className='image-div'>
-                            <img className='wine-img' src={selectedItem?.image}/>
+                            <img className='wine-img' src={selectedItem?.image} alt=''/>
                         </div>
                     </Col>
                     <Col>
@@ -71,14 +73,12 @@ function ItemDetails({selectedItem}){
                                     <div>
                                        <span>Average Rating: </span>
                                        <div class="star-container">
-                                            {stars.map((star, i) => (
+                                            {stars.map((star,i) => (
+                                                // work through the code here!!!!!!
                                             <Star
                                                 key={i}
                                                 starId={i}
-                                                rating={hoverRating || rating}
-                                                onMouseEnter={() => setHoverRating(i)}
-                                                onMouseLeave={() => setHoverRating(0)}
-                                                onClick={() => setRating(i)}
+                                                rating={rating}
                                             />
                                             ))}
                                         </div>
@@ -87,7 +87,7 @@ function ItemDetails({selectedItem}){
                                     <span>{selectedItem?.average_rating}</span>
                                     <span>{selectedItem?.number_of_reviews}</span>
                                 </Row>
-                                <a><u>Add Review</u></a>
+                                {/* <a><u>Add Review</u></a> */}
                             </div>
                         </div>
                     </Col>
